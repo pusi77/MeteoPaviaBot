485ae9735a82602bb6d90b304dc79f847faa2790
pusi77 <pusineriandrea@gmail.com>
2020-03-08 19:54:05 +0100

    3 args bug solved

--- src/main/java/it/pusi77/meteopaviabot/Main.java
+++ src/main/java/it/pusi77/meteopaviabot/Main.java
@@ -23,7 +23,7 @@ public class Main {
         } else if (args[0] != null && args[1] != null) {
             Constants.initConstants(args[0], args[1]);
         }
-        if(args[3] != null) {
+        if(args.length >= 3 && args[3] != null) {
             if (args[3].equals("debug")) {
                 Debug.debug = true;
             }
